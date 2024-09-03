8f8a0046b6a2d8a52687aebe533bd222bb82e366
Mh-Monzil <monzil246@gmail.com>
2024-09-03 12:36:06 +0600

    pipeline updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -411,8 +411,8 @@ const getWatchHistory = asyncHandler(async (req, res) => {
   const user = await User.aggregate([
     {
       $match: {
-        _id: new mongoose.Types.ObjectId(req.user._id)
-      }
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
     },
     {
       $lookup: {
@@ -426,15 +426,34 @@ const getWatchHistory = asyncHandler(async (req, res) => {
               from: "users",
               localField: "owner",
               foreignField: "_id",
-              as: "owner"
-            }
-          }
-        ]
-      }
-    }
-  ])
-})
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
 
+  return res 
+  .status(200)
+  .json(new ApiResponse(200, user[0].watchHistory), "Watch history fetched successfully")
+});
 
 export {
   registerUser,
